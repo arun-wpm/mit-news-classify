2806bfe4e7288f80d57c1fb4b7a1d74497e6b4a0
rayaburong <arun92166@gmail.com>
2020-05-27 23:37:04 +0700

    Debugging TF-IDF + Naive Bayes

--- Arun/Naive_Bayes/nb.py
+++ Arun/Naive_Bayes/nb.py
@@ -3,6 +3,7 @@ Created on Monday May 25 2020 23:27 +0700
 
 @author: arunwpm
 """
+import traceback
 import numpy
 import sys
 sys.path.append("/home/euler/miniconda3/lib/python3.7/site-packages")
@@ -10,10 +11,12 @@ sys.path.append("/home/euler/miniconda3/lib/python3.7/site-packages")
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import train_test_split
 from tf_idf import transform_to_tfidf
 import pandas as pd
 import data_processing as dp
 import pickle
+from nltk.tokenize import RegexpTokenizer
 
 def transform_labels(in_labels):
     # from the input labels, in the format of list of list of labels pertaining to document at row i,
@@ -39,7 +42,7 @@ def transform_labels(in_labels):
         if len(label_matrix[i]) < len(label_to_id):
             label_matrix[i].extend([0]*(len(label_to_id) - len(label_matrix[i])))
 
-    print("Transforming Labels...")
+    print("Transforming Labels...\n")
     return label_matrix, id_to_label
 
 def evaluate(pred, true):
@@ -48,41 +51,66 @@ def evaluate(pred, true):
     for i in range(len(true)):
         for j in range(len(true[i])):
             cnt[true[i][j]][pred[i][j]] += 1
-    print("False Positive Rate ", cnt[0][1]/(cnt[0][1] + cnt[0][0]))
-    print("False Negative Rate ", cnt[1][0]/(cnt[1][0] + cnt[1][1]))
+    
+    f.write("False Positive Rate " + str(cnt[0][1]/(cnt[0][1] + cnt[0][0])) + '\n')
+    f.write("False Negative Rate " + str(cnt[1][0]/(cnt[1][0] + cnt[1][1])) + '\n')
+
+def preprocess(document):
+    document = document.lower()
+    tokenizer = RegexpTokenizer('\w+')
+    return tokenizer.tokenize(document)
 
 if __name__ == "__main__":
-    # the whole corpus
-    with open("../../NYTcorpus.p", "rb") as corpus:
-        all_data = pickle.load(corpus)
-        print("Loaded NYTcorpus!")
-        data = {'Text': []}
-        labels = []
-        for row in all_data:
-            data['Text'].append(row[2])
-            labels.append(row[3:])
-        
-        labels, labels_dict = transform_labels(labels)
-        print(labels_dict)
-        for row in labels:
-            assert(len(row) == len(labels[-1]))
+    f = open("results.txt", 'a+')
+    try:
+        # the whole corpus
+        with open("../../NYTcorpus.p", "rb") as corpus:
+            all_data = pickle.load(corpus)
+            f.write("Loaded NYTcorpus!\n")
+            data = {'Text': []}
+            labels = []
+            for row in all_data:
+                data['Text'].append(row[2])
+                labels.append(row[3:])
+            
+            labels, labels_dict = transform_labels(labels)
+            f.write("Labels dictionary:" + str(labels_dict) + '\n')
+            for row in labels:
+                assert(len(row) == len(labels[-1]))
+            
+            f.write("Labels have been transformed\n")
 
-    # small dataset by Haimoshri
-    # data = pd.read_csv('small_data_h.csv')
-    # data = data.fillna(0)
+        # small dataset by Haimoshri
+        # data = pd.read_csv('small_data_h.csv')
+        # data = data.fillna(0)
 
-    data_train, data_test, labels_train, labels_test = train_test_split(data['Text'], labels, test_size=0.5)
+        # preprocess all the data
+        # for i in range(len(data['Text'])):
+            # data['Text'][i] = preprocess(data['Text'][i])
+        # f.write("Data has been tokenized - no lemmatization at the moment")
 
-    # transform data by tf_idf : tokenize each sample
-    count_vect = CountVectorizer()
-    data_train_counts = count_vect.fit_transform(data_train)
-    data_train = TfidfTransformer().fit_transform(data_train_counts)
-    data_test = TfidfTransformer().transform(data_train_counts)
+        data_train, data_test, labels_train, labels_test = train_test_split(data['Text'], labels, test_size=0.15)
+        f.write("Data has been split into 85% train, 15% test\n")
 
-    # vocab = dp.build_vocabulary({'Text': data_train}) #should use tokenizer rather than re split?
-    # tf_idf = transform_to_tfidf({'Text': data_train}, vocab)
+        # transform data by tf_idf : tokenize each sample
+        count_vect = CountVectorizer()
+        data_train_counts = count_vect.fit_transform(data_train)
+        f.write("Words are tokenized, vocabulary built from training data\n")
+        tfmer = TfidfTransformer()
+        data_train = tfmer.fit_transform(data_train_counts)
+        data_test = tfmer.transform(data_train_counts)
+        f.write("TF-IDF done:" + str(data_train.shape) + ' ' + str(data_test.shape) + '\n')
 
-    ovrnb = OneVsRestClassifier(MultinomialNB()).fit(data_train, labels_train)
-    labels_test_pred = ovrnb.predict(data_test)
+        # vocab = dp.build_vocabulary({'Text': data_train}) #should use tokenizer rather than re split?
+        # tf_idf = transform_to_tfidf({'Text': data_train}, vocab)
 
-    evaluate(labels_test, labels_test_pred)
+        ovrnb = OneVsRestClassifier(MultinomialNB()).fit(data_train, labels_train)
+        f.write("One vs Rest Classfier, Multinomial Naive Bayes fitted data_train\n")
+        labels_test_pred = ovrnb.predict(data_test)
+        f.write("One vs Rest Classfier, Multinomial Naive Bayes predicts labels for data_train\n")
+
+        evaluate(labels_test, labels_test_pred)
+    except Exception:
+        traceback.print_exc(file=f)
+
+    f.close()
