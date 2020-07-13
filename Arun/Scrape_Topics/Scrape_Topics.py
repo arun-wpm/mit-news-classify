906258aea4ba3bb420b88f8fa02920bf0c6ba8ff
rayaburong <arun92166@gmail.com>
2020-07-13 18:54:34 +0700

    Arun: doc2vec and Scrape_topics

---
+++ Arun/Scrape_Topics/Scrape_Topics.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import csv
+import requests
+import pycountry
+import time
+
+WAIT = 10 # stall time so google doesn't block me ;-;
+
+def loadcsv(filename):
+    with open(filename, newline='') as f: 
+        return list(csv.reader(f))
+
+def savecsv(filename, list):
+    with open(filename, "w", newline="") as f:
+        csv.writer(f).writerows(list)
+
+def get_urls(txt):
+    print(txt)
+    soup = BeautifulSoup(txt.text, 'html.parser')
+    links = soup.find_all('a')
+    articles = []
+    for link in links:
+        href = link.get('href')
+        if href[:5] == "/url?": # filter to only those which are links to outside google
+            params = [tuple(x.split('=')) for x in href[5:].split('&')]
+            # print(params)
+            # we only need the q parameter, changeable
+            articles.append([params[0][1]])
+    # each two are duplicates, and the last one is useless
+    return articles[:-1:2]
+
+def craft(base, params):
+    base = base
+    first = True
+    for (x, y) in params:
+        base = base + ("?" if first else "&") + x + "=" + y.lower().replace(' ', '+').replace('_', '+')
+        first = False
+    return base
+
+gurl = "https://www.google.com/search" # "https://www.google.com/search?q=thailand&tbm=nws&start=10"
+gnewsurl = "https://news.google.com/search" # "https://news.google.com/search?q=Thailand&hl=en-US&gl=US&ceid=US%3Aen"
+def query_topic(topic="thailand", n=10):
+    print("Querying for " + topic + ", n=" + str(n))
+    articles = []
+    for i in range(0, n, 10):
+        url = craft(gurl, [("q", topic), ("tbm", "nws"), ("start", str(i))])
+        print(url)
+        page = requests.get(url)
+        articles.extend(get_urls(page))
+        print("stalling...")
+        time.sleep(WAIT)
+    # print("Take a look!")
+    # for url in articles:
+        # print(">> " + url[0])
+    return articles
+
+def query_all(qlist, n=10):
+    print("Start the scraping!")
+    for topic in qlist:
+        if isinstance(topic, tuple):
+            articles = query_topic(topic[1], n)
+            savecsv(topic[0] + ".csv", articles)
+
+        elif isinstance(topic[0], tuple):
+            articles = query_topic(topic[0][1], n)
+            savecsv(topic[0][0] + ".csv", articles)
+        else:
+            articles = query_topic(topic[0], n)
+            savecsv(topic[0] + ".csv", articles)
+        print("more stalling...")
+        time.sleep(WAIT)
+
+def convert_country_names(qlist):
+    print("Converting country codes to names...")
+    for i in range(len(qlist)):
+        code = qlist[i][0]
+        if len(code) > 3 or not code.isupper():
+            continue
+        try:
+            country = pycountry.countries.get(alpha_3=code)
+            qlist[i][0] = (code, country.name)
+        except Exception:
+            continue
+    return qlist
+
+
+if __name__ == "__main__":
+    qlist2 = loadcsv("../../../jiannatopics_named.csv")
+
+    # qlist = loadcsv("../../jiannatopics.csv")
+    # qlist = convert_country_names(qlist)
+
+    query_all(qlist2, 1000)
---
+++ Arun/doc2vec/doc2vec.py
@@ -0,0 +1,102 @@
+"""
+Created Thursday June 18 2020 20:53 +0700
+
+@author: arunwpm
+"""
+import traceback
+import csv
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.utils import tokenize
+
+def loadcsv(filename):
+    with open(filename, newline='') as f: 
+        return list(csv.reader(f))
+
+def savecsv(filename, list):
+    with open(filename, "w", newline="") as f:
+        csv.writer(f).writerows(list)
+
+def transform_labels(in_labels):
+    # from the input labels, in the format of list of list of labels pertaining to document at row i,
+    # output a multilabel matrix where row i column j is 1 if document i contains label j and 0 otherwise
+    label_to_id = {}
+    id_to_label = {}
+    cnt = 0
+    label_matrix = []
+    for row in in_labels:
+        label_matrix.append([])
+        for label in row:
+            if label not in label_to_id:
+                label_to_id[label] = cnt
+                id_to_label[cnt] = label
+                cnt += 1
+            i = label_to_id[label]
+            if i >= len(label_matrix[-1]):
+                label_matrix[-1].extend([0]*(i + 1 - len(label_matrix[-1])))
+            label_matrix[-1][i] = 1
+    
+    for i in range(len(label_matrix)):
+        if len(label_matrix[i]) < len(label_to_id):
+            label_matrix[i].extend([0]*(len(label_to_id) - len(label_matrix[i])))
+
+    return label_matrix, id_to_label
+
+def embed_p(corpusdir="../../", 
+            corpusfile="NYTcorpus.p", 
+            logfile="log.txt", 
+            outfile="embedded.p", 
+            labelfile="binarylabels.p", 
+            labeldictfile="labelsdict.p"):
+    # embed each article in a pickle format corpus into a singular vector using vocabfile as the vocabulary
+    # the format of the corpus is assumed to be the same as NYTcorpus.p
+
+    f = open(logfile, 'a+')
+    f.write("DOC2VEC_EMBED_P\n")
+    try:
+        # load the whole corpus
+        with open(corpusdir + corpusfile, "rb") as corpus:
+            all_data = pickle.load(corpus)
+            f.write("Loaded corpus!\n")
+            data = []
+            labels = []
+            l = len(all_data)
+            for i in range(l):
+                article = all_data[i][2]
+                # tokenize each article
+                data.append(TaggedDocument(list(tokenize(article)), [i])) # article text
+                labels.append(all_data[i][3:]) # article labels
+            label_matrix, label_dict = transform_labels(labels) # make labels binary
+        f.write("Words are tokenized\n")
+
+        # transform data by doc2vec
+        model = Doc2Vec(data)
+        model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
+        vec_data = []
+        for i in range(l):
+            vec_data.append(model.docvecs[i])
+
+        # transform the test data by tf_idf as well
+        f.write("Doc2Vec done\n")
+
+        # save the results into pickle files
+        with open(outfile, "wb") as out:
+            pickle.dump(vec_data, out)
+            f.write("Dumped data output at " + outfile + "\n")
+        
+        with open(labelfile, "wb") as label:
+            pickle.dump(label_matrix, label)
+            f.write("Dumped label output at " + labelfile + "\n")
+
+        with open(labeldictfile, "wb") as labeldict:
+            pickle.dump(label_dict, labeldict)
+            f.write("Dumped label dictionary at " + labeldictfile + "\n")
+        
+    except Exception:
+        traceback.print_exc(file=f)
+
+    f.close()
+
+if __name__ == "__main__":
+    embed_p()
